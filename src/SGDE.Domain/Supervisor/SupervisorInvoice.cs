62c99749d95f7de67fc2d88b1dcf6926367ec172
jsanchez-HOME <jsanchco@gmail.com>
2020-03-25 22:03:16 +0100

    Step 4 in Invoice

--- src/SGDE.Domain/Supervisor/SupervisorInvoice.cs
+++ src/SGDE.Domain/Supervisor/SupervisorInvoice.cs
@@ -7,21 +7,33 @@
     using Converters;
     using Entities;
     using ViewModels;
-    using IronPdf;
     using Newtonsoft.Json.Linq;
     using System.IO;
-
-    //using TheArtOfDev.HtmlRenderer.PdfSharp;
-    //using PdfSharp;
+    using iTextSharp.text;
+    using iTextSharp.text.pdf;
 
     #endregion
 
     public partial class Supervisor
     {
+        public const string _CREATOR = "Servicio de Report (ARCONSA)";
+        public static readonly Font _STANDARFONT_10 = FontFactory.GetFont(FontFactory.HELVETICA, 10);
+        public static readonly Font _STANDARFONT_8 = FontFactory.GetFont(FontFactory.HELVETICA, 8);
+        public static readonly Font _STANDARFONT_10_BOLD = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 10);
+
+        public static readonly Font _STANDARFONT_10_BOLD_CUSTOMCOLOR =
+            FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 10, new BaseColor(144, 54, 25));
+
+        public static readonly Font _STANDARFONT_12_BOLD = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 12);
+        public static readonly Font _STANDARFONT_14_BOLD = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 14);
+
+        public static readonly Font _STANDARFONT_12_BOLD_WHITE =
+            FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 12, BaseColor.White);
+        public static readonly Font _STANDARFONT_14_BOLD_WHITE =
+            FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 14, BaseColor.White);
+
         public InvoiceViewModel GetInvoice(InvoiceQueryViewModel invoiceQueryViewModel)
         {
-            var htmlToPdf = new HtmlToPdf();
-
             var invoiceViewModel = new InvoiceViewModel
             {
                 fileName = $"Fact_{invoiceQueryViewModel.invoiceNumber}.pdf",
@@ -31,66 +43,72 @@
             switch (invoiceQueryViewModel.typeInvoice)
             {
                 case 1:
-                    invoiceViewModel.file = GetInvoiceType1(invoiceQueryViewModel, htmlToPdf);
+                    invoiceViewModel.file = GetInvoiceType1(invoiceQueryViewModel);
                     break;
 
                 default:
-                    invoiceViewModel.file = GetInvoiceType1(invoiceQueryViewModel, htmlToPdf);
+                    invoiceViewModel.file = GetInvoiceType1(invoiceQueryViewModel);
                     break;
             }
 
             return invoiceViewModel;
         }
 
-        private byte[] GetInvoiceType1(InvoiceQueryViewModel invoiceQueryViewModel, HtmlToPdf htmlToPdf)
+        private byte[] GetInvoiceType1(InvoiceQueryViewModel invoiceQueryViewModel)
         {
-            var html = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "assets\\html\\Invoice.html"));
-
-            var listKeyWord = GetKeyWords(html);
-
-            var dictionaryKeyWords = new Dictionary<string, string>();
-            GetDictionaryMyCompany(dictionaryKeyWords);
-            GetDictionaryClient(dictionaryKeyWords, invoiceQueryViewModel);
-
-            foreach (var keyWord in listKeyWord)
+            try
             {
-                if (!dictionaryKeyWords.TryGetValue(keyWord, out string value))
-                    continue;
+                var work = GetWorkById((int)invoiceQueryViewModel.workId);
+                if (work == null)
+                    throw new Exception("Factura mal configurada");
+                var client = GetClientById(work.clientId);
+                if (client == null)
+                    throw new Exception("Factura mal configurada");
 
-                html = html.Replace($"[{keyWord}]", dictionaryKeyWords[keyWord]);
+                var pdf = new Document(PageSize.Letter);
+                Directory.CreateDirectory(Directory.GetCurrentDirectory() + "\\assets\\pdf");
+                var memoryStream = new MemoryStream();
+                var pdfWriter = PdfWriter.GetInstance(pdf, memoryStream);
+
+                pdf.AddTitle("Factura Cliente");
+                pdf.AddCreator(_CREATOR);
+                pdf.Open();
+
+                pdf.Add(GetHeader());                
+                pdf.Add(GetTable_NInvoice(invoiceQueryViewModel, client));
+                pdf.Add(GetTableClient(client, work));
+                pdf.Add(new Paragraph(" ", _STANDARFONT_8));
+                pdf.Add(GetLineSeparator());
+                pdf.Add(new Paragraph(" ", _STANDARFONT_14_BOLD));
+                pdf.Add(GetTableTitle());
+                pdf.Add(new Paragraph(" ", _STANDARFONT_14_BOLD));
+                pdf.Add(GetRowDetailInvoice(invoiceQueryViewModel));
+
+                pdf.Close();
+                pdfWriter.Close();
+
+                return memoryStream.ToArray();
             }
-
-            //CreatePDF(html);
-
-            var pdf = htmlToPdf.RenderHtmlAsPdf(html);
-
-            return pdf.BinaryData;
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
-        private List<string> GetKeyWords(string fileAsString)
+        private PdfPTable GetHeader()
         {
-            var listKeyWords = new List<string>();
+            var pdfPTable = new PdfPTable(2) { WidthPercentage = 100 };
 
-            var findOpen = 0;
-            while (findOpen != -1)
+            var image = Image.GetInstance(Directory.GetCurrentDirectory() + "\\assets\\images\\arconsa.png");
+            image.ScalePercent(75f);
+            var pdfCellImage = new PdfPCell(image)
             {
-                findOpen = fileAsString.IndexOf("[", findOpen);
-                if (findOpen == -1)
-                    break;
+                Rowspan = 4,
+                BorderWidth = 0,
+                PaddingLeft = 20
+            };
+            pdfPTable.AddCell(pdfCellImage);
 
-                var findClose = fileAsString.IndexOf("]", findOpen);
-                var keyWord = fileAsString.Substring(findOpen + 1, (findClose - findOpen) - 1);
-
-                listKeyWords.Add(keyWord);
-
-                if (findOpen != -1) findOpen++;
-            }
-
-            return listKeyWords;
-        }
-
-        private void GetDictionaryMyCompany(Dictionary<string, string> dictionaryKeyWords)
-        {
             var companyData = GetSettingByName("COMPANY_DATA");
             if (companyData == null)
             {
@@ -98,43 +116,200 @@
             }
             var jsonCompanyData = JObject.Parse(companyData.data);
 
-            dictionaryKeyWords.Add("routeImage", Directory.GetCurrentDirectory() + "\\assets\\images\\arconsa.png");
-            dictionaryKeyWords.Add("companyName", jsonCompanyData["companyName"].ToString());
-            dictionaryKeyWords.Add("cif", jsonCompanyData["cif"].ToString());
-            dictionaryKeyWords.Add("address", jsonCompanyData["address"].ToString());
-            dictionaryKeyWords.Add("phoneNumber", jsonCompanyData["phoneNumber"].ToString());
+            var pdfCell = new PdfPCell(new Phrase(jsonCompanyData["companyName"].ToString(), _STANDARFONT_12_BOLD)) { BorderWidth = 0 };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase(jsonCompanyData["cif"].ToString(), _STANDARFONT_12_BOLD)) { BorderWidth = 0 };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase(jsonCompanyData["address"].ToString(), _STANDARFONT_12_BOLD))
+            { BorderWidth = 0 };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase(jsonCompanyData["phoneNumber"].ToString(), _STANDARFONT_12_BOLD)) { BorderWidth = 0 };
+            pdfPTable.AddCell(pdfCell);
+
+            return pdfPTable;
         }
 
-        private void GetDictionaryClient(Dictionary<string, string> dictionaryKeyWords, InvoiceQueryViewModel invoiceQueryViewModel)
+        private PdfPTable GetTable_NInvoice(InvoiceQueryViewModel invoiceQueryViewModel, ClientViewModel client)
         {
-            var work = GetWorkById((int)invoiceQueryViewModel.workId);
-            if (work == null)
-                return;
-            var client = GetClientById(work.clientId);
-            if (work == null)
-                return;
+            var pdfPTable = new PdfPTable(2) { WidthPercentage = 30, HorizontalAlignment = 2 };
 
-            dictionaryKeyWords.Add("clientName", client.name);
-            dictionaryKeyWords.Add("clientAddress", client.address);
-            dictionaryKeyWords.Add("clientPhoneNumber", client.phoneNumber);
-            dictionaryKeyWords.Add("clientWorkName", work.name);
+            var pdfCell = new PdfPCell(new Phrase("Nº Factura", _STANDARFONT_10_BOLD))
+            {
+                BackgroundColor = new BaseColor(204, 204, 255),
+                BorderWidthRight = 0,
+                BorderWidthBottom = 0
+            };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase(invoiceQueryViewModel.invoiceNumber, _STANDARFONT_10_BOLD))
+            {
+                BackgroundColor = new BaseColor(204, 204, 255),
+                BorderWidthLeft = 0,
+                BorderWidthBottom = 0
+            };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase("Fecha", _STANDARFONT_10_BOLD))
+            { BackgroundColor = new BaseColor(204, 204, 255), BorderWidthRight = 0, BorderWidthBottom = 0, BorderWidthTop = 0 };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase(invoiceQueryViewModel.issueDate, _STANDARFONT_10_BOLD))
+            { BackgroundColor = new BaseColor(204, 204, 255), BorderWidthLeft = 0, BorderWidthBottom = 0, BorderWidthTop = 0 };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase("Nº Cliente", _STANDARFONT_10_BOLD))
+            { BackgroundColor = new BaseColor(204, 204, 255), BorderWidthRight = 0, BorderWidthTop = 0 };
+            pdfPTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase(client.id.ToString(), _STANDARFONT_10_BOLD))
+            { BackgroundColor = new BaseColor(204, 204, 255), BorderWidthLeft = 0, BorderWidthTop = 0 };
+            pdfPTable.AddCell(pdfCell);
+
+            return pdfPTable;
         }
 
-        //private void CreatePDF(string html)
-        //{
-        //    //using (System.IO.StreamReader Reader = new System.IO.StreamReader(@"D:\test.html"))
-        //    {
-        //        using (FileStream stream = new FileStream(@"D:\test.pdf", FileMode.Create))
-        //        {
-        //            Document pdfDoc = new Document(PageSize.A4);
-        //            PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
-        //            pdfDoc.Open();
-        //            StringReader sr = new StringReader(html);
-        //            XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
-        //            pdfDoc.Close();
-        //            stream.Close();
-        //        }
-        //    }
-        //}
+        private PdfPTable GetTableClient(ClientViewModel client, WorkViewModel work)
+        {
+            var pdfPTable = new PdfPTable(2) { WidthPercentage = 100 };
+            var widths = new[] { 15f, 42f };
+            pdfPTable.SetWidths(widths);
+
+            var pdfCell = new PdfPCell(new Phrase("Empresa", _STANDARFONT_10_BOLD)) { BorderWidth = 0 };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase(client.name, _STANDARFONT_10)) { BorderWidth = 0 };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase("CIF/NIF", _STANDARFONT_10_BOLD)) { BorderWidth = 0 };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase(client.cif, _STANDARFONT_10)) { BorderWidth = 0 };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase("Dirección", _STANDARFONT_10_BOLD)) { BorderWidth = 0 };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase(client.address, _STANDARFONT_10)) { BorderWidth = 0 };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase("Teléfono", _STANDARFONT_10_BOLD)) { BorderWidth = 0 };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase(client.phoneNumber, _STANDARFONT_10)) { BorderWidth = 0 };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase("Obra", _STANDARFONT_10_BOLD)) { BorderWidth = 0 };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase(work.name, _STANDARFONT_10)) { BorderWidth = 0 };
+            pdfPTable.AddCell(pdfCell);
+
+            return pdfPTable;
+        }
+
+        private PdfPTable GetTableTitle()
+        {
+            var pdfPTable = new PdfPTable(4) { WidthPercentage = 100 };
+            var widths = new[] { 40f, 20f, 20f, 20f };
+            pdfPTable.SetWidths(widths);
+
+            var pdfCell = new PdfPCell(new Phrase("SERVICIOS REALIZADOS", _STANDARFONT_12_BOLD_WHITE))
+            {
+                BackgroundColor = new BaseColor(20, 66, 92),
+                HorizontalAlignment = Element.ALIGN_CENTER,
+                VerticalAlignment = Element.ALIGN_MIDDLE,
+                PaddingTop = 2f,
+                PaddingBottom = 6f,
+                BorderWidth = 0
+            };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase("UNIDAD", _STANDARFONT_12_BOLD_WHITE))
+            {
+                BackgroundColor = new BaseColor(20, 66, 92),
+                HorizontalAlignment = Element.ALIGN_CENTER,
+                VerticalAlignment = Element.ALIGN_MIDDLE,
+                PaddingTop = 2f,
+                PaddingBottom = 6f,
+                BorderWidth = 0
+            };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase("PRECIO UNIDAD", _STANDARFONT_12_BOLD_WHITE))
+            {
+                BackgroundColor = new BaseColor(20, 66, 92),
+                HorizontalAlignment = Element.ALIGN_CENTER,
+                VerticalAlignment = Element.ALIGN_MIDDLE,
+                PaddingTop = 2f,
+                PaddingBottom = 6f,
+                BorderWidth = 0
+            };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase("IMPORTE", _STANDARFONT_12_BOLD_WHITE))
+            {
+                BackgroundColor = new BaseColor(20, 66, 92),
+                HorizontalAlignment = Element.ALIGN_CENTER,
+                VerticalAlignment = Element.ALIGN_MIDDLE,
+                PaddingTop = 2f,
+                PaddingBottom = 6f,
+                BorderWidth = 0
+            };
+            pdfPTable.AddCell(pdfCell);
+
+            return pdfPTable;
+        }
+
+        private PdfPTable GetRowDetailInvoice(InvoiceQueryViewModel invoiceQueryViewModel)
+        {
+            var listMonths = new [] { "ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO", "JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE" };
+            var monthIni = DateTime.ParseExact(invoiceQueryViewModel.startDate, "d/MM/yyyy", null).Month; 
+            var monthEnd = DateTime.ParseExact(invoiceQueryViewModel.endDate, "d/MM/yyyy", null).Month;
+            var yearIni = DateTime.ParseExact(invoiceQueryViewModel.startDate, "d/MM/yyyy", null).Year;
+            var yearEnd = DateTime.ParseExact(invoiceQueryViewModel.endDate, "d/MM/yyyy", null).Year;
+
+            var title = "";
+            if (monthIni == monthEnd && yearIni == yearEnd)
+            {
+                title = $"HORAS POR ADMINISTRACION SEGÚN SERVICIOS PRESTADOS EN LA OBRA DE REFERENCIA CORRESPONIENTES AL MES DE {listMonths[monthIni - 1]} {yearIni}";
+            }
+            else
+            {
+                title = $"HORAS POR ADMINISTRACION SEGÚN SERVICIOS PRESTADOS EN LA OBRA DE REFERENCIA CORRESPONIENTES ENTRE LOS MESES DE {listMonths[monthIni - 1]} {yearIni} Y {listMonths[monthEnd - 1]} {yearEnd}";
+            }
+
+            var pdfPTable = new PdfPTable(4) { WidthPercentage = 100 };
+            var widths = new[] { 40f, 20f, 20f, 20f };
+            pdfPTable.SetWidths(widths);
+
+            var pdfCell = new PdfPCell(new Phrase(title, _STANDARFONT_10))
+            {
+                HorizontalAlignment = Element.ALIGN_LEFT,
+                VerticalAlignment = Element.ALIGN_MIDDLE,
+                PaddingTop = 2f,
+                PaddingBottom = 6f,
+                BorderWidth = 0
+            };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase(" ", _STANDARFONT_10))
+            {
+                HorizontalAlignment = Element.ALIGN_LEFT,
+                VerticalAlignment = Element.ALIGN_MIDDLE,
+                PaddingTop = 2f,
+                PaddingBottom = 6f,
+                BorderWidth = 0
+            };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase(" ", _STANDARFONT_10))
+            {
+                HorizontalAlignment = Element.ALIGN_LEFT,
+                VerticalAlignment = Element.ALIGN_MIDDLE,
+                PaddingTop = 2f,
+                PaddingBottom = 6f,
+                BorderWidth = 0
+            };
+            pdfPTable.AddCell(pdfCell);
+            pdfCell = new PdfPCell(new Phrase(" ", _STANDARFONT_10))
+            {
+                HorizontalAlignment = Element.ALIGN_LEFT,
+                VerticalAlignment = Element.ALIGN_MIDDLE,
+                PaddingTop = 2f,
+                PaddingBottom = 6f,
+                BorderWidth = 0
+            };
+            pdfPTable.AddCell(pdfCell);
+
+            return pdfPTable;
+        }
+
+        private Chunk GetLineSeparator()
+        {
+            return new Chunk(
+                new iTextSharp.text.pdf.draw.LineSeparator(0f, 100f, BaseColor.LightGray, Element.ALIGN_LEFT, 1));
+        }
     }
 }
