eceb4d26a95edfcfb87d841e635716e58c58bd6f
jsanchez-HOME <jsanchco@gmail.com>
2020-03-27 20:38:28 +0100

    Add migration AccountUser to User

---
+++ src/SGDE.DataEFCoreSQL/Migrations/20200327192739_Add_Field_AccountNumber.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SGDE.DataEFCoreSQL.Migrations
+{
+    public partial class Add_Field_AccountNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AccountNumber",
+                table: "User",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccountNumber",
+                table: "User");
+        }
+    }
+}
