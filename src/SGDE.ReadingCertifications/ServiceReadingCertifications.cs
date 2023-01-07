ff107d7149a9833335124681117df6ad09cfa015
jsanchco <jsanchco@gmail.com>
2023-01-07 07:45:40 +0100

    First commit

---
+++ src/SGDE.ReadingCertifications/ServiceReadingCertifications.cs
@@ -0,0 +1,41 @@
+﻿using ExcelDataReader;
+using System.Data;
+using System.IO;
+
+namespace SGDE.ReadingCertifications
+{
+    public class ServiceReadingCertifications
+    {
+        public void ReadFile()
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            var filePath = "c:\\Temp\\CERT N°4 OFERTA 133.V1D.2022.xls";
+            using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+                var reader = ExcelDataReader.ExcelReaderFactory.CreateReader(stream);
+                //var conf = new ExcelDataSetConfiguration
+                //{
+                //    ConfigureDataTable = _ => new ExcelDataTableConfiguration
+                //    {
+                //        UseHeaderRow = true
+                //    }
+                //};
+
+                var dataSet = reader.AsDataSet();
+
+                var dataTable = dataSet.Tables[0];
+                var rows = dataTable.Rows;
+                var numColumns = dataTable.Columns.Count;
+                foreach (DataRow row in rows)
+                {
+                    for (int col = 0; col < numColumns; col++)
+                    {
+                        var cell = row[col].ToString();
+                        System.Diagnostics.Debug.Write($"{cell};");
+                    }
+                    System.Diagnostics.Debug.WriteLine(string.Empty);
+                }
+            }
+        }
+    }
+}
---
+++ src/SGDE.Test/ReadingCerifications/ReadingCerticationsTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SGDE.ReadingCertifications;
+
+namespace SGDE.Tests.ReadingCerifications
+{
+    [TestClass]
+    public class ReadingCerticationsTests
+    {
+        [TestMethod]
+        public void ReadFile_ResultOk()
+        {
+            var serviceReadingCertifications = new ServiceReadingCertifications();
+            serviceReadingCertifications.ReadFile();
+
+            //Assert.AreNotEqual(newUserViewModel.roleId, 0);
+        }
+    }
+}
