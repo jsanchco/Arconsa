71d4efc8b204bbcefeb32f75c2d830345f63b17a
Jesús Sánchez Corzo <jesus.sanchezc@codere.com>
2023-02-13 20:47:34 +0100

    Add Partidas

---
+++ src/SGDE.Domain/Entities/Partida.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace SGDE.Domain.Entities
+{
+    public class Partida : BaseEntity
+    {
+        public string Codigo { get; set; }
+        public string Orden { get; set; }
+        public string Descripcion { get; set; }
+        public string Unidades { get; set; }
+        public double? UnidadesCertificacionAnterior { get; set; }
+        public double? UnidadesCertificacionActual { get; set; }
+        public double? UnidadesPresupuesto { get; set; }
+        public double? PresupuestoCapitulo { get; set; }
+        public string Type { get; set; }
+
+        public virtual ICollection<Partida> SubCapitulos { get; set; } = new HashSet<Partida>();
+    }
+}
---
+++ src/SGDE.Domain/ViewModels/PartidaViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SGDE.Domain.ViewModels
+{
+    public class PartidaViewModel : BaseEntityViewModel
+    {
+        public string codigo { get; set; }
+        public string orden { get; set; }
+        public string descripcion { get; set; }
+        public string unidades { get; set; }
+        public double? unidadesCertificacionAnterior { get; set; }
+        public double? unidadesCertificacionActual { get; set; }
+        public double? unidadesPresupuesto { get; set; }
+        public double? presupuestoCapitulo { get; set; }
+        public string type { get; set; }
+    }
+}
